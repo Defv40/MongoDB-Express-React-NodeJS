7d2956733f4c9fc74c54df59b6aeb12c883e12cd
German Popov <ProgDefv@gmail.com>
2023-07-20 00:28:21 +0400

    Edit readme Add token for auth user

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Полный Full Stack курс ReactJS + NodeJS  (MongoDB, Express, React, NodeJS)
 
 ## Blog mern
+
+
+### Разворачиваем проект 
+1. clone project
+2. open project in terminal
+3. type in terminal: npm install 
+4. npm run dev
--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 import express from 'express';
-
+import jwt from 'jsonwebtoken'
 const app = express();
+
+app.use(express.json()); // учим читать json
+
+app.get('/', (req, res) =>{
+    res.send('hello user you"re noob');
+})
+
+app.post('/auth/login', (req, res) =>{
+    const {email, password} = req.body;
+    const privateSecretKey = 'asdflsdkfkld';
+    const token = jwt.sign({
+        email,
+        password
+    }, privateSecretKey);
+
+    res.json({
+        token
+    })
+})
+
+app.listen(3000, (err) =>{
+    if (err){
+        return console.log(err)
+    }
+    console.log('ok');
+})
